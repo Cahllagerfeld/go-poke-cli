3194cfd5cd14ec5973c4f4f8bf7d55c69cae3ada
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-03-27 15:20:52 +0000

    refactor: application structure

--- internal/core/services/pokemon/pokemonsrv.go
+++ internal/core/services/pokemon/pokemonsrv.go
@@ -1,6 +1,12 @@
 package pokemonsrv
 
-import "github.com/cahllagerfeld/go-poke-cli/internal/core/domain"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/cahllagerfeld/go-poke-cli/internal/core/domain"
+)
 
 type Service struct{}
 
@@ -8,6 +14,23 @@ func New() *Service {
 	return &Service{}
 }
 
-func (serv *Service) Get(id int) (domain.Pokemon, error) {
-	return domain.Pokemon{}, nil
+func (serv *Service) GetOne(id int) (*domain.Pokemon, error) {
+	url := fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%d", id)
+
+	target := &domain.Pokemon{}
+
+	client := &http.Client{}
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch data: %w", err)
+	}
+	defer res.Body.Close()
+
+	err = json.NewDecoder(res.Body).Decode(target)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to decode response: %w", err)
+	}
+	return target, nil
 }
--- internal/ports/ports.go
+++ internal/ports/ports.go
@@ -3,5 +3,5 @@ package ports
 import "github.com/cahllagerfeld/go-poke-cli/internal/core/domain"
 
 type PokemonService interface {
-	Get(id int) (domain.Pokemon, error)
+	GetOne(id int) (*domain.Pokemon, error)
 }
