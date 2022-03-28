ac0d65da8ea3038ae735e946c6a01fb8f496e956
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-03-28 17:20:52 +0000

    feat: add main method

--- internal/handlers/cli/cli.go
+++ internal/handlers/cli/cli.go
@@ -1,18 +1,36 @@
 package cli
 
 import (
-	"github.com/cahllagerfeld/go-poke-cli/internal/core/domain"
-	"github.com/cahllagerfeld/go-poke-cli/internal/ports"
+	"fmt"
+	"strconv"
+
+	pokemonsrv "github.com/cahllagerfeld/go-poke-cli/internal/core/services/pokemon"
+	"github.com/spf13/cobra"
 )
 
 type Handler struct {
-	pokeService ports.PokemonService
+	pokeService *pokemonsrv.Service
 }
 
-func New(p *ports.PokemonService) *Handler {
-	return &Handler{pokeService: *p}
+func New(p *pokemonsrv.Service) *Handler {
+	return &Handler{pokeService: p}
 }
 
-func (h *Handler) GetOne(id int) (*domain.Pokemon, error) {
-	return &domain.Pokemon{}, nil
+func (h *Handler) GetOne() *cobra.Command {
+	return &cobra.Command{
+		Use:   "get",
+		Args:  cobra.ExactArgs(1),
+		Short: "Get Pokemon Info by number",
+		Run: func(cmd *cobra.Command, args []string) {
+			id, err := strconv.Atoi(args[0])
+			if err != nil {
+				panic("Failed to parse ID")
+			}
+			data, err := h.pokeService.GetOne(id)
+			if err != nil {
+				fmt.Println("No data")
+			}
+			fmt.Println(data.Name)
+		},
+	}
 }
